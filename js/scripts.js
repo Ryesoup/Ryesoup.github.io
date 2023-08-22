a11e3da853560340c8dbcf98d12aacbdcc710570
Rye <rye1@Ryeui-MacBookAir.local>
2023-08-22 12:32:04 +0900

    test5

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ $(document).ready(function() {
     $("#header").load("/partials/header.html");
     $("#footer").load("/partials/footer.html");
 });
+
+function openNav() {
+    document.querySelector(".sidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.querySelector(".sidebar").style.width = "0";
+}
