4b42c035a94c836da86d15058e5b717dc0656e83
Rye <rye1@Ryeui-MacBookAir.local>
2023-08-22 14:41:29 +0900

    230822 1441

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-    $("#header").load("/partials/header.html");
-    $("#footer").load("/partials/footer.html");
-});
+    $("#header").load("partials/header.html", function() {
+        window.onscroll = function() {
+            const topbar = document.querySelector('.topbar');
+            if (window.scrollY > 50) {
+                topbar.classList.add('scrolled');
+            } else {
+                topbar.classList.remove('scrolled');
+            }
+        };
+    });
 
-window.onscroll = function() {
-    const topbar = document.querySelector('.topbar');
-    if (window.scrollY > 50) {
-        topbar.classList.add('scrolled');
-    } else {
-        topbar.classList.remove('scrolled');
-    }
-};
+    $("#footer").load("partials/footer.html");
+});
