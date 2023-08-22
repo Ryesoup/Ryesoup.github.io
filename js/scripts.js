f39e00b9f664f750f49b1e87eb1311a52eed5bf8
Rye <rye1@Ryeui-MacBookAir.local>
2023-08-22 13:53:24 +0900

    test14

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,8 @@ $(document).ready(function() {
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.querySelector(".content").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.querySelector(".content").style.marginLeft = "0";
 }
