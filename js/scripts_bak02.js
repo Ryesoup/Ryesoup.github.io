131ee1c711ba41d143c5e215c7aa95708dabb5a2
Ryesoup <85975787+Ryesoup@users.noreply.github.com>
2023-08-23 15:18:05 +0900

    230823 1518

--- js/scripts_bak02.js
+++ js/scripts_bak02.js
@@ -1,25 +1,25 @@
+// scripts.js
+
 document.addEventListener('DOMContentLoaded', function() {
     // Mobile navigation toggle
     let navToggle = document.getElementById('nav-toggle');
     let mainNav = document.getElementById('main-nav');
 
-    if(navToggle && mainNav) {
-        navToggle.addEventListener('click', function() {
-            mainNav.classList.toggle('active');
-        });
+    navToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+    });
 
-        // Overlay for mobile menu
-        let overlay = document.querySelector('.overlay');
+    // Overlay for mobile menu
+    let overlay = document.querySelector('.overlay');
 
-        navToggle.addEventListener('click', function() {
-            overlay.classList.toggle('active');
-        });
+    navToggle.addEventListener('click', function() {
+        overlay.classList.toggle('active');
+    });
 
-        overlay.addEventListener('click', function() {
-            overlay.classList.remove('active');
-            mainNav.classList.remove('active');
-        });
-    }
+    overlay.addEventListener('click', function() {
+        overlay.classList.remove('active');
+        mainNav.classList.remove('active');
+    });
 
     // Dropdown functionality
     let dropdownTriggers = document.querySelectorAll('.dropdown > a, .nested-dropdown > a');
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
     dropdownTriggers.forEach(function(trigger) {
         trigger.addEventListener('click', function(event) {
             event.preventDefault();
+
             let content = this.nextElementSibling;
             if (content.classList.contains('active')) {
                 content.classList.remove('active');
@@ -34,25 +35,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.querySelectorAll('.dropdown-content.active, .nested-content.active').forEach(function(activeContent) {
                     activeContent.classList.remove('active');
                 });
+
                 content.classList.add('active');
             }
         });
     });
-});
-
-$(document).ready(function() {
-    $("#header").load("partials/header.html", function() {
-        window.onscroll = function() {
-            const topbar = document.querySelector('.topbar');
-            if (window.scrollY > 50) {
-                topbar.classList.add('scrolled');
-            } else {
-                topbar.classList.remove('scrolled');
-            }
-        };
-    });
-
-    $("#footer").load("partials/footer.html");
 });
 
 // You can continue with other scripts if any
