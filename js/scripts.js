0201ec121648341086a3cc2ac0c65efc40fdae5b
Ryesoup <85975787+Ryesoup@users.noreply.github.com>
2023-08-25 14:33:00 +0900

    230825 1432

--- js/scripts.js
+++ js/scripts.js
@@ -41,19 +41,18 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function bindDropdownEvents() {
-    const dropdowns = document.querySelectorAll('.dropdown');
+    const mainDropdown = document.querySelector('.dropdown');
     const nestedDropdowns = document.querySelectorAll('.nested-dropdown');
     const overlay = document.querySelector('.overlay');  
 
-    dropdowns[0].addEventListener('mouseover', function() {
-        // Blog를 hover할 때 모든 nested-dropdowns를 표시
+    mainDropdown.addEventListener('mouseenter', function() {
         this.querySelectorAll('.nested-content').forEach(nestedContent => {
             nestedContent.classList.add('active');
         });
         overlay.style.display = 'block';
     });
 
-    dropdowns[0].addEventListener('mouseout', function() {
+    mainDropdown.addEventListener('mouseleave', function() {
         this.querySelectorAll('.nested-content').forEach(nestedContent => {
             nestedContent.classList.remove('active');
         });
@@ -61,18 +60,11 @@ function bindDropdownEvents() {
     });
 
     nestedDropdowns.forEach((nestedDropdown) => {
-        nestedDropdown.addEventListener('mouseover', function() {
-            // 모든 nested-dropdowns 항목들을 비활성화
-            dropdowns[0].querySelectorAll('.nested-content').forEach(nestedContent => {
+        nestedDropdown.addEventListener('mouseenter', function() {
+            mainDropdown.querySelectorAll('.nested-content').forEach(nestedContent => {
                 nestedContent.classList.remove('active');
             });
-            // 현재 nested-dropdown 항목만 활성화
             this.querySelector('.nested-content').classList.add('active');
-        });
-
-        nestedDropdown.addEventListener('mouseout', function() {
-            // 선택하지 않아도 되므로 이 부분은 그대로 둡니다.
-            this.querySelector('.nested-content').classList.remove('active');
         });
     });
 }
